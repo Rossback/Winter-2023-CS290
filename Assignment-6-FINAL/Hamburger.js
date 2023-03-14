f0838181ed987dcb1db1013f1817553eb6969bc5
Rossback <103798290+Rossback@users.noreply.github.com>
2023-03-14 12:13:58 -0700

    Adding Stuff to hopefully get the WAMP server to update...?

---
+++ Assignment-6-FINAL/Hamburger.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("hidelinks");
+      if (x.style.display === "none") {
+        x.className == "showlinks";
+      } else {
+        x.className == "hidelinks";
+      }
+}
